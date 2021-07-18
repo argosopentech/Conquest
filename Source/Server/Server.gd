19ebf14ad0c1d9020d05e9332a04438724a9bb1a
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2021-07-18 19:50:51 +0500

    Create/Update/Join Lobbies

--- Source/Server/Server.gd
+++ Source/Server/Server.gd
@@ -3,6 +3,8 @@ extends Node
 const SERVER_IP = "127.0.0.1"
 const SERVER_PORT = 1909
 var player_name = "player"
+var my_lobby = {}
+var active_lobbies = {}
 
 func _ready():
 	connect_signals()
@@ -30,6 +32,30 @@ func connect_to_server():
 	else:
 		print("Error connecting to server: ", str(error))
 
-remote func send_player_data():
+remote func send_player_name():
 	var sender_id = get_tree().get_rpc_sender_id()
-	rpc_id(sender_id, "get_player_data", player_name)
+	rpc_id(sender_id, "get_player_name", player_name)
+	print("Sent player name")
+
+func create_lobby(lobby_data):
+	rpc_id(1, "create_lobby", lobby_data)
+
+remote func lobby_created(lobby_data):
+	my_lobby = lobby_data
+
+func join_lobby(lobby_code, lobby_pass):
+	rpc_id(1, "join_lobby", lobby_code, lobby_pass)
+
+remote func update_lobby(lobby_data, reason = ""):
+	my_lobby = lobby_data
+	print(reason)
+
+remote func failed_to_join_lobby(reason = ""):
+	print(reason)
+
+func ask_for_active_lobbies():
+	rpc_id(1, "send_active_lobbies")
+
+remote func get_active_lobbies(lobbies):
+	active_lobbies = lobbies
+	print("Got active lobbies.")
