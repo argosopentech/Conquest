c7e2df26e2f89742c5a125c14f85789edc3c1945
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2023-02-14 21:17:28 +0500

    Low level websockets server code

---
+++ Source/Server/WebSocketsServer.gd
@@ -0,0 +1,67 @@
+extends Node
+class_name WebSocketsServer
+
+var client = WebSocketClient.new()
+var response_handler = null
+
+signal server_connected
+signal server_disconnected
+
+func _ready():
+    set_process(false)
+
+func set_response_handler(new_handler):
+    response_handler = new_handler
+
+func connect_to_server(server_address="ws://localhost:9080"):
+    if client.connect_to_url(server_address) == OK:
+        connect_server_signals()
+        set_process(true)
+    else:
+        set_process(false)
+
+func connect_server_signals():
+	if client.is_connect("connection_established", self, "connected_to_server"):
+        return
+    client.connect("connection_established", self, "connected_to_server")
+    client.connect("connection_closed", self, "disconnected_from_server")
+	client.connect("connection_error", self, "disconnected_from_server")
+	client.connect("data_received", self, "received_data_from_server")
+
+func connected_to_server():
+    print("Connected to the server.")
+    emit_signal(server_connected)
+
+func disconnected_from_server():
+    print("Disconnected from the server.")
+    emit_signal(server_connected)
+
+func received_data_from_server():
+    var method_info = client.get_peer(1).get_var()
+    print("Just received data from server %s" % [method_info])
+    process_method_info(method_info)
+
+func process_method_info(method_info):
+    if !response_handler: return
+    if method_info is Dictionary & method_info.has("purpose") & method_info["purpose"] == "request":
+        if request_handler.has_method(method_info["method"]):
+            request_handler.call_deferred(method_info["method"], id, method_info["data"])
+
+func _process():
+    client.poll()
+
+func send_data_to_server(method, data=null):
+    var method_info = {"purpose": "request", "method": method, "data": data}
+    client.get_peer(1).put_var(method_info, true)
+
+func disconnect_from_server():
+    disconnect_server_signals()
+    client.disconnect_from_host()
+
+func disconnect_server_signals():
+	if !client.is_connect("connection_closed", self, "_closed"):
+        return
+	client.disconnect("connection_closed", self, "disconnected_from_server")
+	client.disconnect("connection_error", self, "disconnected_from_server")
+	client.disconnect("connection_established", self, "connected_to_server")
+	client.disconnect("data_received", self, "received_data_from_server")
